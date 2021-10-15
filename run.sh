8c153535aeae1d3a3b8cf266f130e2b52ea0a117
siw36 <rhh.klussmann@gmail.com>
2021-10-15 17:41:01 +0200

    added SSH directory to run script

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 CODE=${HOME}/Code
 docker run --rm --name ansible-development -d \
   -v ${CODE}:/home/${USER}/code \
+  -v ${HOME}/.ssh:/home/${USER}/.ssh \
   --user $UID:$UID \
   ansible-development:latest
