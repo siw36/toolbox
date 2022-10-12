0771851729a28bea3c68c619f76585b3f8cd06d9
siw36 <rhh.klussmann@gmail.com>
2022-10-12 14:11:23 +0200

    added velero cli and made image build architecture agnostic

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,7 @@
 docker build \
   --build-arg USER_ID=$UID \
   --build-arg USER_NAME=$USER \
+  --build-arg ARCHITECTURE=$(uname -m) \
   --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
   -t toolbox:latest \
   .
